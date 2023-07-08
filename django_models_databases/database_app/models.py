3ae33191a836b3f0a14311c16582188a5af8963d
Gamze Yücel <yucelgamze99@icloud.com>
2023-07-08 17:18:46 +0300

    database models

---
+++ django_models_databases/database_app/models.py
@@ -0,0 +1,14 @@
+from typing import Any
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+class Musician(models.Model):
+    name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=30)
+    age = models.IntegerField(validators=[MinValueValidator(0),MinValueValidator(150)])
+    salary = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    
+    def __str__(self):
+        return f"Name: {self.name}, Instrument: {self.instrument}, Age: {self.age}, Salary: {self.salary}"
+    
